273ae241f4e565085fe56e786052ac92769fc1b7
Andrew White <white.d.andrew@gmail.com>
2023-05-03 02:17:16 -0400

    Update executor.py

--- rmrkl/executor.py
+++ rmrkl/executor.py
@@ -19,31 +19,5 @@ class ExceptionTool(BaseTool):
 
 class RetryAgentExecutor(AgentExecutor):
     """Agent executor that retries on output parser exceptions."""
-
-    def _take_next_step(
-        self,
-        name_to_tool_map: Dict[str, BaseTool],
-        color_mapping: Dict[str, str],
-        inputs: Dict[str, str],
-        intermediate_steps: List[Tuple[AgentAction, str]],
-        run_manager: Optional[CallbackManagerForChainRun] = None,
-    ) -> Union[AgentFinish, List[Tuple[AgentAction, str]]]:
-        try:
-            return super()._take_next_step(
-                name_to_tool_map, color_mapping, inputs, intermediate_steps
-            )
-        except OutputParserException as e:
-            # ok, this isn't great - I agree
-            # but not sure of a more clean way to get this
-            text = str(e).split("`")[1]
-            observation = "Invalid or incomplete response"
-            agent_action = AgentAction("_Exception", observation, text)
-            self.callback_manager.on_agent_action(
-                agent_action, verbose=self.verbose, color="red"
-            )
-            tool_run_kwargs = self.agent.tool_run_logging_kwargs()
-            # trigger run to get bookkeeping on basetool callback manager
-            ExceptionTool().run(
-                observation, verbose=self.verbose, color="red", **tool_run_kwargs
-            )
-            return [(agent_action, observation)]
+    # for backwards compatibility
+    handle_parsing_errors: bool = True
