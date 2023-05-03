a14de974475e4626a3c14f0a646b86c7ab30ee55
Andrew White <white.d.andrew@gmail.com>
2023-05-03 02:01:54 -0400

    Update executor.py

--- rmrkl/executor.py
+++ rmrkl/executor.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Tuple, Union, Optional
 
 from langchain.agents import AgentExecutor
 from langchain.schema import AgentAction, AgentFinish, OutputParserException
